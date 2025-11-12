49f91c1117461fcf35de0a823ab217b560c2ace3
Frank Wang <1454884738@qq.com>
2025-11-12 10:08:44 -0800

    Restore AtomHttpApi and AtomRpc modules (needs porting to Signals)

--- packages/atom/src/index.ts
+++ packages/atom/src/index.ts
@@ -14,6 +14,16 @@ export * as AtomRef from "./AtomRef.js"
 /**
  * @since 1.0.0
  */
+export * as AtomHttpApi from "./AtomHttpApi.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as AtomRpc from "./AtomRpc.js"
+
+/**
+ * @since 1.0.0
+ */
 export * as Registry from "./Registry.js"
 
 /**
